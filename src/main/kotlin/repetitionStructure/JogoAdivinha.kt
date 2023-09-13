bf027db885e69b1f16ded1e40bf28dc9d19bca0c
eduardo <edu.santos023@hotmail.com>
2023-09-13 16:33:36 -0300

    Commit de atualização do jogo de adivinhação

--- src/main/kotlin/repetitionStructure/JogoAdivinha.kt
+++ src/main/kotlin/repetitionStructure/JogoAdivinha.kt
@@ -12,8 +12,8 @@ fun main () {
         val enter = readln().toInt()
 
         when {
-            (enter > secret) -> println("O número é menor")
-            (enter < secret) -> println("O número é maior")
+            (enter > secret) -> println("O número é menor!")
+            (enter < secret) -> println("O número é maior!")
             else -> {
                 println("Parabéns você acertou, o número é $secret!")
                 //break faz o loop ser falso e encerrar o jogo
